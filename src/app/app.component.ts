d60214376da9b241abb3ea39eee4ecabe5b51f95
Ryan Lester <hacker@linux.com>
2017-10-19 13:19:58 -0400

    balls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as test63987 from 'test63987';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  test63987 = JSON.stringify(test63987);
 }
